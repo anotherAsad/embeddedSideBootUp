c951db20192f2e45067bbc5986987e10e5b57f0d
anotherAsad <ashaq.bee16seecs@seecs.edu.pk>
2021-01-18 19:43:00 +0500

    Arduino UNO Serial Tx/Rx Loop Code

--- code.c
+++ code.c
@@ -1,5 +1,14 @@
-#define BASE 0x40000000
-
 int main(){
-	*((int*)BASE) = 0x7fff;
+	// Setup
+	char recv;
+	init();
+	Serial.begin(9600);
+	Serial.write("1234");
+	// Loop
+	while(1){
+		recv = Serial.read();
+		recv = recv == '9' ? '0' : recv + 1;
+		Serial.write(recv);
+		delay(1000);
+	}
 }
